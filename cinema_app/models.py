e46af07b4e972f2c98698e1f75b3c6cc1df37895
kubix <maks05102000@gmail.com>
2023-02-09 18:20:34 +0100

    added 2 models Category and Product

--- cinema_app/models.py
+++ cinema_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    VAT = [(0.23, '0.23'),
+           (0.08, '0.08'),
+           (0.05, '0.05'),
+           (0, '0'),]
+
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.FloatField()
+    vat = models.FloatField(choices=VAT)
+    stock = models.PositiveIntegerField()
+    categories = models.ManyToManyField(Category)
