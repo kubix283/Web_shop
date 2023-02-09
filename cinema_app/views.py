251057299af9fc046114215f1b4cc7960d1cf271
kubix <maks05102000@gmail.com>
2023-02-09 18:51:07 +0100

    Added CategoryListView and it is sorted by name, added it o urls and created urls in cinema_app and created category_list.html

--- cinema_app/views.py
+++ cinema_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category
 
-# Create your views here.
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        return Category.objects.all().order_by('category_name')
+
