0072cab74dd43a76bcb4f892f779d95547b0ffc2
kubix <maks05102000@gmail.com>
2023-02-12 17:06:47 +0100

    added product detail html view and urls

--- cinema_app/urls.py
+++ cinema_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, category_view, product_detail
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='categories'),
+    path('category/<slug:slug>/', category_view, name='category'),
+    path('product/<int:pk>', product_detail, name='product'),
 ]
--- cinema_app/views.py
+++ cinema_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Category
+from .models import Category, Product
 
 class CategoryListView(ListView):
     model = Category
@@ -10,3 +10,19 @@ class CategoryListView(ListView):
     def get_queryset(self):
         return Category.objects.all().order_by('category_name')
 
+
+
+def category_view(request, slug):
+    category = Category.objects.get(slug=slug)
+    products = Product.objects.filter(categories=category).order_by('name')
+    for product in products:
+        product.price_brutto = product.price * (1 + product.vat)
+
+    context = {'products': products, 'category': category}
+    return render(request, 'category_detail.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    categories = product.categories.all()
+    return render(request, 'product_detail.html', {'product': product, 'categories': categories})
